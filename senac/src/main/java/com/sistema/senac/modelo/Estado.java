80397dddc432f2c78475e37de8147b967a10392c
NICOLLAS GUILHERME GREGORIO <nicollas.4909@aluno.pr.senac.br>
2023-08-16 10:18:13 -0300

    subindo sistema

---
+++ senac/src/main/java/com/sistema/senac/modelo/Estado.java
@@ -0,0 +1,42 @@
+package com.sistema.senac.modelo;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Estado implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String nome;
+	private String sigla;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
